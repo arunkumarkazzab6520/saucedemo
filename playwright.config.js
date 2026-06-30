593aa0ebab46e26c9b3e3278620aff147dbe8230
unknown <arunkumarkazzab6520>
2026-06-30 19:47:56 +0530

    retries added on config file

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-  },
+    retries : 1,
+    },
     //timeout: 3000,
   /* Configure projects for major browsers */
   projects: [
